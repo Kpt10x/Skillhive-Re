d67bc31064f083a454c430799944d05e5b4cf7cf
Krishna Prasad Tripathy <work.tripathykrishna@gmail.com>
2024-12-30 00:03:20 +0530

    Commited sprint1
    Sprint1 work uploaded Team3

--- SkillHive Learning Nexus- Transforming Skills Into Mastery1/SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
+++ SkillHive Learning Nexus- Transforming Skills Into Mastery1/SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './instructor/components/dashboard/dashboard.component';
+import { ViewAssignedCoursesComponent } from './instructor/components/view-assigned-courses/view-assigned-courses.component.spec';
+import { CreateInstructorComponent } from './instructor/components/create-instructor/create-instructor.component';
+import { ViewInstructorComponent } from './instructor/components/view-instructor/view-instructor.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'view-assigned-courses', component: ViewAssignedCoursesComponent },
+  { path: 'create-instructor', component: CreateInstructorComponent },
+  {path:'view-instructor',component:ViewInstructorComponent}
+  
+];
