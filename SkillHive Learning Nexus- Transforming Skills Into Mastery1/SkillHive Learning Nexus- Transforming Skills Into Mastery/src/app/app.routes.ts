2fcd9794c5cfabbe6dbe88b13a4b27db36e37ceb
Sampreeth <sampreeth.personal@gmail.com>
2024-12-31 09:39:17 +0530

    Pushed Sprint 1 Files.

--- SkillHive Learning Nexus- Transforming Skills Into Mastery1/SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
+++ SkillHive Learning Nexus- Transforming Skills Into Mastery1/SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './authentication/components/login/login.component';
+import { HomeComponent } from './authentication/components/home/home.component';
+import { AdminDashboardComponent } from './authentication/components/admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent },
+  {path: 'home', component: HomeComponent},
+  {path: 'admin-dashboard', component: AdminDashboardComponent}
+  // { path: '**', redirectTo: 'login' }
+];
