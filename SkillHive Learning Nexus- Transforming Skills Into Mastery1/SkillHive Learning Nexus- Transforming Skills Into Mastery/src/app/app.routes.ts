588a6dbf24b308f6b3ecc8fe842e3e251bfd8d3a
Sampreeth <sampreeth.personal@gmail.com>
2025-01-06 22:35:34 +0530

    Fixed routing issues

--- SkillHive Learning Nexus- Transforming Skills Into Mastery1/SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
+++ SkillHive Learning Nexus- Transforming Skills Into Mastery1/SkillHive Learning Nexus- Transforming Skills Into Mastery/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/components/login/login.component';
-import { HomeComponent } from './authentication/components/home/home.component';
 import { AdminDashboardComponent } from './authentication/components/admin-dashboard/admin-dashboard.component';
 
 export const appRoutes: Routes = [
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: 'login', component: LoginComponent },
-  {path: 'home', component: HomeComponent},
-  {path: 'admin-dashboard', component: AdminDashboardComponent}
-  // { path: '**', redirectTo: 'login' }
+  {path: 'admin-dashboard', component: AdminDashboardComponent},
+  { path: '**', redirectTo: 'login' }
 ];
